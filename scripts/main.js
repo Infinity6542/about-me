12590b5f81d12d19fbce6fb5eca5182f527e8fee
unknown <isd6542@outlook.com>
2025-01-21 16:37:47 +1100

    Made preloader

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,35 @@
-import { animate } from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
+import {
+	animate,
+	stagger,
+} from "https://cdn.jsdelivr.net/npm/motion@latest/+esm";
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const circle = document.getElementById("followCursor");
+
+const loaderSequence = [
+	[
+		"#loading > span",
+		{ filter: "blur(0px)" },
+		{
+			delay: stagger(2),
+			ease: "cubic-bezier(.31,-0.01,.07,1.02)",
+			duration: 1,
+		},
+	],
+	[
+		"#followCursor",
+		{ height: "10px", width: "10px" },
+		{ delay: 2, ease: "cubic-bezier(.31,-0.01,.07,1.02)", duration: 1 },
+	],
+];
+sleep(2000).then(async () => {
+	await animate(loaderSequence).then(() =>
+		document.querySelector("#loading").classList.add("hidden")
+	);
+});
 window.onpointermove = (event) => {
 	const { clientX, clientY } = event;
 
