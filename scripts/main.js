0f241a3edc802c89aef4472c73f1f0797dc49be7
unknown <isd6542@outlook.com>
2025-04-19 14:40:47 +1000

    Fixed bug where the circle would take a while before moving

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,10 @@ const circle = document.getElementById("followCursor");
 let buddy = addEventListener("mousemove", async (event) => {
 	const { clientX, clientY } = event;
 
-	await circle.animate(
+	circle.style.left = `${clientX}px`;
+	circle.style.top = `${clientY}px`;
+
+	circle.animate(
 		{
 			left: `${clientX}px`,
 			top: `${clientY}px`,
